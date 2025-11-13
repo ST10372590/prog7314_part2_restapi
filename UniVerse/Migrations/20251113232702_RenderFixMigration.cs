cb4c87bd13f2bc1d0df8b9a3c9f2ea49088ffdba
ST10372590 <127498518+ST10372590@users.noreply.github.com>
2025-11-14 01:28:41 +0200

    Added migration for Render deployment

---
+++ UniVerse/Migrations/20251113232702_RenderFixMigration.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniVerse.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenderFixMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DeviceToken",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserID",
+                keyValue: 1,
+                column: "DeviceToken",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserID",
+                keyValue: 2,
+                column: "DeviceToken",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserID",
+                keyValue: 3,
+                column: "DeviceToken",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserID",
+                keyValue: 4,
+                column: "DeviceToken",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeviceToken",
+                table: "Users");
+        }
+    }
+}
